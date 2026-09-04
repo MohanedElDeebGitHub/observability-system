572d48a13fe1c5bc6978faed971708e43591c667
Mohaned <mohanedeldeeb27@gmail.com>
2026-09-04 19:50:45 +0300

    fe, git ignore, and docker files

--- observability-system/src/main/java/com/mohaned/observability_system/model/LogEntity.java
+++ observability-system/src/main/java/com/mohaned/observability_system/model/LogEntity.java
@@ -1,12 +1,14 @@
 package com.mohaned.observability_system.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Entity
 @Table(name = "logs")
--- observability-system/src/main/java/com/mohaned/observability_system/sse/SseService.java
+++ observability-system/src/main/java/com/mohaned/observability_system/sse/SseService.java
@@ -1,12 +1,14 @@
 package com.mohaned.observability_system.sse;
 
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@Service
 public class SseService {
     private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
